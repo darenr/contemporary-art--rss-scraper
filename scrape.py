3ab86fc90897e69a4b6c87b8af331b3ead31443c
drace <daren.race@oracle.com>
2016-10-18 10:53:57 -0700

    better working wiyh image selector

--- scrape.py
+++ scrape.py
@@ -10,10 +10,15 @@ import requests_cache
 import feedparser
 import collections
 from bs4 import BeautifulSoup
+from urlparse import urlparse, urljoin
 
 one_day = 60 * 60 * 24
 requests_cache.install_cache(
     'rss_cache', backend='sqlite', expire_after=one_day)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0'
+}
 
 def get_entry_formatted(mime_type, value):
     if mime_type.lower() == 'text/html':
@@ -25,7 +30,6 @@ def get_entry_formatted(mime_type, value):
 def parse_content(mime_type, value):
     if mime_type.lower() == 'text/html':
         soup = BeautifulSoup(value, 'html5lib')
-
 
         # scoop up all the text
         result = {
@@ -52,23 +56,29 @@ def get_entry_value(entry, key, feed):
 
 def fetch_page_and_parse(feed, url):
     print ' *', 'parsing page link:', url
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, 'html5lib')
+    page = requests.get(url, headers=headers)
     result = {}
 
-    if 'find_image_selector' in feed:
-        for el in soup.findAll(feed['find_image_selector']['element'], {"class": feed['find_image_selector']['class']}):
-            for img in el.findAll('img'):
-                if img['src'].startswith('/'):
-                    result['imgurl'] = feed['find_image_selector']['root'] + img['src']
-                else:
-                    result['imgurl'] = img['src']
-                break
-    else:
-        # look for og_image as the default
-        if soup.find('meta', {"property": "og:image"}):
-            if 'content' in soup.find('meta', {"property": "og:image"}):
-                result['imgurl'] = soup.find('meta', {"property": "og:image"})['content']
+    if page.status_code == 200:
+
+        soup = BeautifulSoup(page.text, 'html5lib')
+
+        if 'selector' in feed:
+            for img in soup.select(feed['selector']):
+                src = img['src'] if img.has_attr('src') else None
+                if not src:
+                    src = img['srcset'] if img.has_attr('srcset') else None
+                if src:
+                    if src.startswith('/'):
+                        result['imgurl'] = urljoin(feed['url'], src)
+                    else:
+                        result['imgurl'] = src
+                    break
+        else:
+            # look for og_image as the default
+            if soup.find('meta', {"property": "og:image"}):
+                if 'content' in soup.find('meta', {"property": "og:image"}):
+                    result['imgurl'] = soup.find('meta', {"property": "og:image"})['content']
 
     return result
 
@@ -84,7 +94,9 @@ def validate(record):
 
 
 def process_feed(feed):
-    rawxml = requests.get(feed['url'])
+    print ' *', 'processing', feed['url']
+
+    rawxml = requests.get(feed['url'], headers=headers)
     d = feedparser.parse(rawxml.text)
     for entry in d['entries']:
 
@@ -104,18 +116,15 @@ def process_feed(feed):
 
         if 'summary_detail' in entry and entry['summary_detail']:
             m = parse_content(entry["summary_detail"]["type"], entry["summary_detail"]["value"])
-            record["description_snippet"] = m['text']
+            if 'text' in m:
+                record["description"] = m['text']
             if 'imgurl' in m:
-                record['imgurl'] = m['imgurl']
+                record["imgurl"] = m['imgurl']
 
-        if 'content' in entry and entry['content']:
-            m = parse_content(entry["content"][0]["type"], entry["content"][0]["value"])
-            record["description"] = m['text']
-            if 'imgurl' in m:
-                record['imgurl'] = m['imgurl']
-
-        if 'media_content' in entry and entry['media_content'][0]['type'] == 'image/jpeg':
-            record['imgurl'] = entry['media_content'][0]['url']
+        if 'media_thumbnail' in entry and entry['media_thumbnail']:
+            media_thumbnail = entry['media_thumbnail'][0]
+            if 'url' in media_thumbnail:
+                record["imgurl"] = media_thumbnail['url']
 
         if 'tags' in entry and entry['tags']:
             for x in entry['tags']:
@@ -124,25 +133,20 @@ def process_feed(feed):
 
         record['user_tags'] = list(set(record['user_tags']))
 
-        if 'replacement' in feed:
-            for k in feed['replacement']:
-                record[k] = feed['replacement'][k][0] % (record[feed['replacement'][k][1]])
-                record[feed['replacement'][k][1]] = ""
-
-
         if not record['imgurl']:
-            for k, v in fetch_page_and_parse(feed, record['link']):
-                record[k] = v
+            m = fetch_page_and_parse(feed, record['link'])
+            for k in m:
+                record[k] = m[k]
 
         if not validate(record):
-            print json.dumps(record, indent=True)
-            print '-'*60
+            #print json.dumps(record, indent=True)
+            #print '-'*60
             for k in entry:
                 print k, '\t\t', entry[k]
 
-        sys.exit(0)
+            sys.exit(0)
 
-        #print record['imgurl']
+        print record['imgurl']
         #print '-'*60
 
 
@@ -153,7 +157,7 @@ if __name__ == "__main__":
 
     try:
         for feed in sources['feeds']:
-            if feed["organization"] == "ArtsJournal":
+            if feed["organization"] == "Nowness":
                 process_feed(feed)
 
     except Exception, e:
